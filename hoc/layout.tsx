4afb3be72b6681eb329de7b1fe351b1d7442ab56
CptKokao <pro-markov@yandex.ru>
2021-11-27 22:11:52 +0300

    + createContext

--- hoc/layout.tsx
+++ hoc/layout.tsx
@@ -1,12 +1,16 @@
-import { FunctionComponent } from "react";
-import { Layout } from "../layout/layout";
+import { FunctionComponent } from 'react';
+import { IAppContext } from '../context/app.context';
+import { AppContextProvider } from '../context/app.context';
+import { Layout } from '../layout/layout';
 
-export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
-  return function withLayout(props: T): JSX.Element {
-    return (
-      <Layout>
-        <Component {...props} />
-      </Layout>
-    );
-  };
-};
+export const withLayout = <T extends Record<string, unknown> & IAppContext>(Component: FunctionComponent<T>) => {
+	return function withLayoutComponent(props: T): JSX.Element {
+		return (
+			<AppContextProvider menu={props.menu} firstCategory={props.firstCategory}>
+				<Layout>
+					<Component {...props} />
+				</Layout>
+			</AppContextProvider>
+		);
+	};
+}; 
--- layout/sidebar/sidebar.tsx
+++ layout/sidebar/sidebar.tsx
@@ -1,11 +1,13 @@
 import { SidebarProps } from './sidebar.props';
 import styles from './p.module.css';
 import cn from 'classnames';
+import { Menu } from '../menu/menu';
 
 export const Sidebar = ({ ...props}: SidebarProps): JSX.Element => {
   return (
     <div {...props}>
       Sidebar
+      <Menu />
     </div>
   );
 };
